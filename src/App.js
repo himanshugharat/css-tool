2ec3829464f7f9c4960bab15ec3e2c4f440c069f
himanshu <himanshugharat44@gmail.com>
2021-04-24 20:54:31 +0530

    added: the quote route to project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setdata] = useState([]);
+  const [loading, setloading] = useState(true);
+  useEffect(async () => {
+    await fetch("https://animechan.vercel.app/api/random")
+      .then((response) => response.json())
+      .then((quote) => setdata(quote));
+  }, [loading]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <button
+        className="animeButton"
+        onClick={() => {
+          setloading(!loading);
+        }}
+      >
+        More
+      </button>
+      <div class="grid-container mainCard">
+        <div class="name">{data.anime}</div>
+        <div class="content">{data.quote}</div>
+        <div class="by">-{data.character}</div>
+      </div>
     </div>
   );
 }
