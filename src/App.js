10588b87a0e735056f951e156033e7595fe380fc
himanshu <himanshugharat44@gmail.com>
2021-05-03 15:47:28 +0530

    added: filter for invert to the project

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Contrast from "./pages/filter/Contrast";
 import Dropshadow from "./pages/filter/Dropshadow";
 import Grayscale from "./pages/filter/Grayscale";
 import HueRotate from "./pages/filter/HueRotate";
+import Invert from "./pages/filter/Invert";
 
 function App() {
   return (
@@ -31,7 +32,8 @@ function App() {
       {/* <Contrast /> */}
       {/* <Dropshadow /> */}
       {/* <Grayscale /> */}
-      <HueRotate />
+      {/* <HueRotate /> */}
+      <Invert />
     </div>
     // <div className="grid-container">
     //   <div className="header">
---
+++ src/pages/filter/Invert.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import Button from "../../component/Button";
+import tile from "../../style/gradient.module.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import image from "../../assets/images/templete.svg";
+function Invert() {
+  const [invert, setInvert] = useState(100);
+
+  const notify = () =>
+    toast.info("copied to clipboard", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  return (
+    <div className={tile.gridContainer}>
+      <div className={tile.input}>
+        <div className={tile.card}>
+          <label htmlFor="amount">Amount</label>
+          <div>
+            <input
+              type="range"
+              max="100"
+              min="1"
+              value={invert}
+              onChange={(e) => {
+                setInvert(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className={tile.output}>
+        <div className={tile.card}>
+          <div
+            style={{
+              backgroundImage: `url(${image})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "contain",
+              filter: `invert(${invert}%)`,
+              height: "200px",
+              width: "250px",
+            }}
+          ></div>
+        </div>
+      </div>
+      <div className={tile.clipboard}>
+        <div className={tile.card}>
+          <div>filter: {`invert(${invert}%)`}</div>
+          <Button
+            name="copy"
+            operation={() => {
+              navigator.clipboard.writeText(`filter: invert(${invert}%)`);
+              notify();
+            }}
+          />
+        </div>
+      </div>
+      <div className={tile.additional}></div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
+  );
+}
+
+export default Invert;
